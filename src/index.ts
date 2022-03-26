e4edf8e9be8f5fc9fecece4713b5c272224f4f34
Poppy <a.fraser.howard@gmail.com>
2022-03-26 18:15:20 +0800

    Orbit Cam, Axes Helper, and Floor Plane

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,47 @@
-import * as THREE from 'three';
+import { 
+    AxesHelper, 
+    BoxGeometry, 
+    Mesh, MeshBasicMaterial, 
+    PerspectiveCamera, 
+    PlaneGeometry, 
+    Scene, 
+    WebGLRenderer 
+} from 'three';
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls'
 import './style.css';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// Y is up
+// X is right
+// Z is depth.
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const scene = new Scene();
 
-camera.position.z = 5;
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(5, 5, 5);
+camera.lookAt(0,0,0);
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.target.set( 0, 0.5, 0 );
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = true;
+
+const axesHelper = new AxesHelper( 5 );
+axesHelper.position.set(-5,0,-5);
+scene.add(axesHelper);
+
+scene.add(makeFloor());
+
+const geometry = new BoxGeometry();
+const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new Mesh( geometry, material );
+scene.add( cube );
 
 function animate() {
     requestAnimationFrame( animate );
@@ -22,3 +50,15 @@ function animate() {
     renderer.render( scene, camera );
 }
 animate();
+
+function makeFloor() {
+    const geometry = new PlaneGeometry(5, 5, 1, 1);
+    const material = new MeshBasicMaterial( { color: 0xffffff })
+    const plane = new Mesh( geometry, material );
+    plane.rotateX(dtr(-90));
+    return plane;
+}
+
+function dtr(degrees: number) {
+    return degrees * (Math.PI / 180);
+}
