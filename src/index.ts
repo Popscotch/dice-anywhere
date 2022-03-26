1b16866d72581b205c8449e7e123f38c322ba9a0
Poppy <a.fraser.howard@gmail.com>
2022-03-26 23:15:11 +0800

    Refactor for physics

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,72 @@
 import * as THREE from 'three';
-import {
-    OrbitControls
-} from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { Colors } from './colors';
 import { Materials } from './materials';
 import './style.css';
 
-// Renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-document.body.appendChild( renderer.domElement );
+let camera: THREE.Camera | THREE.PerspectiveCamera;
+let scene: THREE.Scene;
+let renderer: THREE.WebGLRenderer;
+let controls;
+let cube: THREE.Mesh<THREE.BoxGeometry, THREE.MeshPhysicalMaterial>;
 
-// Scene
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xb5c9e8);
+init();
+animate();
 
-// Camera
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(5, 5, 5);
-camera.lookAt(0,0,0);
+function init() {
+    initGraphics();
+    createObjects();
+}
 
-// Orbit Control
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.target.set( 0, 0.5, 0 );
-controls.update();
-controls.enablePan = false;
-controls.enableDamping = true;
+function initGraphics() {
+    // Camera
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.position.set(5, 5, 5);
+    camera.lookAt(0,0,0);
 
-// Light
-const light = new THREE.PointLight( Colors.White, 1, 100);
-light.position.set(1, 4, -1);
-light.castShadow = true;
-scene.add(light);
-scene.add( new THREE.AmbientLight(Colors.DarkGrey));
+    // Scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xb5c9e8);
 
-// Floor Plane
-const plane = new THREE.Mesh( new THREE.PlaneGeometry(5, 5, 1, 1), Materials.Standard());
-plane.receiveShadow = true;
-plane.castShadow = true;
-plane.rotateX(dtr(-90));
-scene.add(plane);
+    // Renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    document.body.appendChild( renderer.domElement );
 
-// Cube
-const cube = new THREE.Mesh( new THREE.BoxGeometry(), Materials.Standard() );
-cube.material.color = Colors.Blue
-cube.position.set(0, 1, 0);
-cube.castShadow = true;
-cube.receiveShadow = true;
-scene.add( cube );
+    // Orbit Control
+    controls = new OrbitControls( camera, renderer.domElement );
+    controls.target.set( 0, 0.5, 0 );
+    controls.update();
+    controls.enablePan = false;
+    controls.enableDamping = true;
+
+    // Ambient Light
+    scene.add( new THREE.AmbientLight(Colors.DarkGrey));
+
+    // Light
+    const light = new THREE.PointLight( Colors.White, 1, 100);
+    light.position.set(1, 4, -1);
+    light.castShadow = true;
+    scene.add(light);
+}
+
+function createObjects() {
+    // Floor Plane
+    const plane = new THREE.Mesh( new THREE.PlaneGeometry(5, 5, 1, 1), Materials.Standard());
+    plane.receiveShadow = true;
+    plane.castShadow = true;
+    plane.rotateX(dtr(-90));
+    scene.add(plane);
+
+    cube = new THREE.Mesh( new THREE.BoxGeometry(), Materials.Standard() );
+    cube.material.color = Colors.Blue
+    cube.position.set(0, 1, 0);
+    cube.castShadow = true;
+    cube.receiveShadow = true;
+    scene.add(cube);
+}
 
 // Animate Scene
 function animate() {
@@ -58,7 +75,6 @@ function animate() {
     cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
-animate();
 
 // Degree's to radians
 function dtr(degrees: number) {
